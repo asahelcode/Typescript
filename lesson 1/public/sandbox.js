d84adec18e30f87d75d39c4a7a8a08ede8704a09
asahelcode <hoyx0101@gmail.com>
2022-09-24 12:32:32 +0000

    simulate a account credition

--- lesson 1/public/sandbox.js
+++ lesson 1/public/sandbox.js
@@ -5,4 +5,9 @@ const logDetails = (uid, item) => {
 const greet = (user) => {
     console.log(`${user.name} is the daedarus of code`);
 };
+const creditAccount = (account, amount) => {
+    account.balance += amount;
+    console.log(`Your account has successfully been credited with ${amount}, balance N${account.balance}`);
+};
 greet({ name: 'henry', uid: '3849309049' });
+creditAccount({ name: 'Henry', balance: 560000, guarantor: 'Blend', passcode: '4559' }, 450830);
--- lesson 1/src/sandbox.ts
+++ lesson 1/src/sandbox.ts
@@ -1,5 +1,11 @@
 type strOrNum = string | number;
 type userObject = { name: string; uid: strOrNum };
+type bankAccount = {
+  name: string;
+  balance: number;
+  guarantor: string;
+  passcode: string;
+};
 
 const logDetails = (uid: strOrNum, item: string) => {
   console.log(`${item} has a uid of ${uid}`);
@@ -9,4 +15,16 @@ const greet = (user: userObject) => {
   console.log(`${user.name} is the daedarus of code`);
 };
 
+const creditAccount = (account: bankAccount, amount: number): void => {
+  account.balance += amount;
+  console.log(
+    `Your account has successfully been credited with ${amount}, balance N${account.balance}`
+  );
+};
+
 greet({ name: 'henry', uid: '3849309049' });
+
+creditAccount(
+  { name: 'Henry', balance: 560000, guarantor: 'Blend', passcode: '4559' },
+  450830
+);
